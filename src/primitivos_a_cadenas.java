fed9304a709d6ff9c4cd839ae0ca1dd74ce68594
DiAlPiRo <diegop.rosero@gmail.com>
2022-07-07 16:11:37 -0500

    convertir primitivos a cadenas

---
+++ src/primitivos_a_cadenas.java
@@ -0,0 +1,14 @@
+public class primitivos_a_cadenas {
+    public static void main(String[] args) {
+        boolean cierto=true;
+        String ciertoConvertido=Boolean.toString(cierto);
+        int entero=5687;
+        String enteroConvertido=Integer.toString(entero);
+        float flotante=2.56e2f;
+        String flotanteConvertido=Float.toString(flotante);
+        double decimal=23.678;
+        String decimalConvertido=Double.toString(decimal);
+        String mensaje = ciertoConvertido+"\n"+enteroConvertido+"\n"+flotanteConvertido+"\n"+decimalConvertido;
+        System.out.println(mensaje);
+    }
+}
