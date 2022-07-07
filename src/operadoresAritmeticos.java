9393a2027a9f78c7f6ba04d346097c147eb42efd
DiAlPiRo <diegop.rosero@gmail.com>
2022-07-07 16:40:32 -0500

    operadores aritmeticos

---
+++ src/operadoresAritmeticos.java
@@ -0,0 +1,17 @@
+public class operadoresAritmeticos {
+    public static void main(String[] args) {
+        int a=5,b=7;
+        int suma=a+b;
+        int resta=a-b;
+        int multiplicacion=a*b;
+        float division=(float)a/(float)b;
+        int resto=a%b;
+        System.out.println("suma= "+suma);
+        System.out.println("resta= "+resta);
+        System.out.println("multiplicacion= "+multiplicacion);
+        System.out.println("division= "+division);
+        System.out.println("resto= "+resto);
+
+
+    }
+}
